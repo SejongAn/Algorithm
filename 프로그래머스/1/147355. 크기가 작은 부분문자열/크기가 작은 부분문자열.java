8534b2445f0a30e8ec3375ef51121a5d93324113
SejongAn <155035916+SejongAn@users.noreply.github.com>
2024-03-13 23:32:32 +0900

    Update 크기가 작은 부분문자열.java

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -1,14 +1,14 @@
 class Solution {
     public int solution(String t, String p) {
-        long p_num=Long.parseLong(p);
-        System.out.println(p_num);
+        //Integer.parseInt() 그대로 사용하면서 앞만 long으로 바꾸는 바보 짓 함.
+
+        long p_num=Long.parseLong(p); //p -> Long으로 전환
         int p_len=p.length();
-        System.out.println(p_len);
         int answer = 0; 
-        for (int i =0; i<t.length()-(p_len-1);i++){
-            long t_num=Long.parseLong(t.substring(i,i+p_len));
-            if(t_num<=p_num){
-                System.out.println(t_num);
+        
+        for (int i =0; i<t.length()-(p_len-1);i++){ //p의 자릿수가 여러개일 경우 그 갯수-1만큼 for문을 덜 돌려야함
+            long t_num=Long.parseLong(t.substring(i,i+p_len)); //t에서 p의 자릿수에 맞게 부분 문자열 출력 -> 숫자로 전환
+            if(t_num<=p_num){ //전환한 숫자 t_num과 p_num 비교
                 answer ++;
             }
         }

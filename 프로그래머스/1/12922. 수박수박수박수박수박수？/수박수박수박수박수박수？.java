78ea9f2a592a447afb292527f8743daf595a0516
SejongAn <155035916+SejongAn@users.noreply.github.com>
2024-03-18 18:42:23 +0900

    Update 수박수박수박수박수박수？.java

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -2,11 +2,11 @@ class Solution {
     public String solution(int n) {
         String answer = "";
         for(int i=0;i<n;i++){
-            if(i%2==0){ //
+            if(i%2==0){ //홀수면
                 //add 박
                 answer = answer + '수';
             }
-            else{ //홀수면
+            else{ //짝수면
                 //add 수
                 answer = answer + '박';
             }
